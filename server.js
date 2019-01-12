d501a07da01d3f378c2ae8b25ee4ea6b7bc252aa
JeiWolfei <jvampy22@gmail.com>
2019-01-12 13:59:14 -0800

    server updates

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 var http = require("http");
-
- /*one way to set up the server*/
-
- /*http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);*/
+var url = require("url");
 
  function start() {
   function onRequest(request, response) {
-    console.log("Request received");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+    route(handle, pathname, response, request);
   }
+  
   http.createServer(onRequest).listen(8888);
   console.log("Server has started.");
 }
